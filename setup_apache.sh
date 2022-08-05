9c14e66f9c350b3c11e4f1ba14990132a9802c6b
Brian Rogers <67665681+BrianMRogers@users.noreply.github.com>
2022-08-05 18:14:54 -0400

    Update setup_apache.sh

--- setup_apache.sh
+++ setup_apache.sh
@@ -2,4 +2,4 @@
 set -x
 sudo apt update
 sudo apt install -y  apache2
-echo "/var/webserver_log 192.168.1.2(rw,sync,no_root_squash,no_subtree_check)" | sudo tee -a /etc/exports
+echo "/var/webserver_log 192.168.1.2(rw,sync,no_root_squash,no_subtree_check)" | sudo tee -a /etc/exports > /etc
