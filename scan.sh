35f181b80807c0be4b23156808bf8abd9c5e36a8
Brian Rogers <67665681+BrianMRogers@users.noreply.github.com>
2022-08-05 23:54:47 -0400

    Update scan.sh

--- scan.sh
+++ scan.sh
@@ -1,2 +1,2 @@
 sudo grep Invalid /var/log/auth.log | grep sshd > output
-cut -d " " -f 1-3,11 output > /var/webserver_log/unauthorized.log
+sudo cut -d " " -f 1-3,11 output > /var/webserver_log/unauthorized.log
