00942308edf751f0aea0bfc1c4034ccb282772c3
Brian Rogers <67665681+BrianMRogers@users.noreply.github.com>
2022-08-06 04:05:57 -0400

    Update scan.sh

--- scan.sh
+++ scan.sh
@@ -1,3 +1,20 @@
 sudo grep Invalid /var/log/auth.log | grep sshd > output
 sudo cut -d " " -f 1-3,11 output > /var/webserver_log/unauthorized.log
 cat /var/webserver_log/unauthorized.log
+
+ipArr=()
+monthArr=()
+dayArr=()
+countryArr=()
+
+while read -r line; do   monthArr+= cut -d " " -f 1 dayArr+= cut -d " " -f 2 echo "$line" ; done < output
+
+#Translate IP using geoiplookup library
+
+#For loop for amount of elements found display elements in order at i
+
+#Finally clear arrays when finished
+ipArr=()
+monthArr=()
+dayArr=()
+countryArr=()
